f955927ba851e29dfc84cebdf7e55bca7b3a07f6
Darcy Shen <da@liii.pro>
2026-05-17 14:25:24 +0800

    [0132] 放大缩小时跟随光标 (#3379)

--- src/Edit/Interface/edit_interface.cpp
+++ src/Edit/Interface/edit_interface.cpp
@@ -165,11 +165,38 @@ edit_interface_rep::get_pixel_size () {
 }
 
 void
+compute_zoom_scroll (SI cursor_x, SI cursor_y, SI old_sx, SI old_sy,
+                     double old_magf, double new_magf, SI& new_sx, SI& new_sy) {
+  new_sx= cursor_x - (SI) tm_round ((cursor_x - old_sx) * old_magf / new_magf);
+  new_sy= cursor_y - (SI) tm_round ((cursor_y - old_sy) * old_magf / new_magf);
+}
+
+void
 edit_interface_rep::set_zoom_factor (double zoom) {
+  double old_magf= magf;
+
+  // 获取旧视口中央（在更新 magf 之前，确保逻辑坐标一致）
+  SI old_cx= 0, old_cy= 0;
+  if (is_attached (this) && old_magf > 0.0) {
+    update_visible ();
+    old_cx= (vx1 + vx2) / 2;
+    old_cy= (vy1 + vy2) / 2;
+  }
+
   zoomf = zoom;
   magf  = zoomf / std_shrinkf;
   pixel = (SI) tm_round ((std_shrinkf * PIXEL) / zoomf);
   zpixel= max ((SI) tm_round (std_shrinkf * PIXEL), pixel);
+
+  if (is_attached (this) && old_magf > 0.0 && magf > 0.0) {
+    cursor cu= get_cursor ();
+    if (cu->valid) {
+      SI new_cx, new_cy;
+      compute_zoom_scroll (cu->ox, cu->oy, old_cx, old_cy, old_magf, magf,
+                           new_cx, new_cy);
+      scroll_to (new_cx, new_cy);
+    }
+  }
 }
 
 void
