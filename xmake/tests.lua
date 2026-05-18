2b9a8a79119b001cd822d63661e4119a76997d79
Darcy Shen <da@liii.pro>
2026-05-18 19:49:31 +0800

    [0138] xmake r dddd 不会卡住 (#3392)

--- xmake/tests.lua
+++ xmake/tests.lua
@@ -111,9 +111,9 @@ function add_target_scheme_test(filepath, INSTALL_DIR, RUN_ENVS)
             print("Executing: " .. regtest_name)
             params = {
                 "-headless",
+                "-d",
                 "-b", filepath,
-                "-x", regtest_name,
-                "-q"
+                "-x", "(catch #t (lambda () " .. regtest_name .. " (quit-TeXmacs)) (lambda args (display \"Error: \") (display args) (newline) (exit 1)))"
             }
             if is_plat("macosx", "linux") then
                 binary = target:deps()["stem"]:targetfile()
@@ -148,9 +148,9 @@ function add_target_integration_test(filepath, INSTALL_DIR, RUN_ENVS)
             print("Executing: " .. test_name)
             params = {
                 "-headless",
+                "-d",
                 "-b", path.join("TeXmacs","tests",name..".scm"),
-                "-x", test_name,
-                "-q"
+                "-x", "(catch #t (lambda () " .. test_name .. " (quit-TeXmacs)) (lambda args (display \"Error: \") (display args) (newline) (exit 1)))"
             }
             if is_plat("macosx", "linux") then
                 binary = target:deps()["stem"]:targetfile()
